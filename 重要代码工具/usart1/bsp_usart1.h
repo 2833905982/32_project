2ae68b1001c4df4783747fb46177c74b2146be0f
万人迷-渣子哥 <44614119+2833905982@users.noreply.github.com>
2019-10-30 00:20:29 +0800

    路

--- 重要代码工具/usart1/bsp_usart1.h
+++ 重要代码工具/usart1/bsp_usart1.h
@@ -1,5 +1,5 @@
-#ifndef __USART_H
-#define	__USART_H
+#ifndef __USART1_H
+#define	__USART1_H
 
 
 #include "stm32f10x.h"
@@ -11,23 +11,25 @@
 	* 2-修改GPIO的宏
   */
 	
+	 /*初始化USART1 配置模式为 115200 8-N-1，中断接收*/
+	
 // 串口1-USART1
-#define  DEBUG_USARTx                   USART1
-#define  DEBUG_USART_CLK                RCC_APB2Periph_USART1
-#define  DEBUG_USART_APBxClkCmd         RCC_APB2PeriphClockCmd
-#define  DEBUG_USART_BAUDRATE           115200
+#define  DEBUG_USART1                   USART1
+#define  DEBUG_USART1_CLK                RCC_APB2Periph_USART1
+#define  DEBUG_USART1_APBxClkCmd         RCC_APB2PeriphClockCmd
+#define  DEBUG_USART1_BAUDRATE           115200
 
 // USART GPIO 引脚宏定义
-#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOA)
-#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+#define  DEBUG_USART1_GPIO_CLK           (RCC_APB2Periph_GPIOA)
+#define  DEBUG_USART1_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
     
-#define  DEBUG_USART_TX_GPIO_PORT       GPIOA   
-#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_9
-#define  DEBUG_USART_RX_GPIO_PORT       GPIOA
-#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_10
+#define  DEBUG_USART1_TX_GPIO_PORT       GPIOA   
+#define  DEBUG_USART1_TX_GPIO_PIN        GPIO_Pin_9
+#define  DEBUG_USART1_RX_GPIO_PORT       GPIOA
+#define  DEBUG_USART1_RX_GPIO_PIN        GPIO_Pin_10
 
-#define  DEBUG_USART_IRQ                USART1_IRQn
-#define  DEBUG_USART_IRQHandler         USART1_IRQHandler
+#define  DEBUG_USART1_IRQ                USART1_IRQn
+#define  DEBUG_USART1_IRQHandler         USART1_IRQHandler
 
 
 // 串口2-USART2
@@ -104,9 +106,9 @@
 //#define  DEBUG_USART_IRQHandler         UART5_IRQHandler
 
 
-void USART_Config(void);
-void Usart_SendByte( USART_TypeDef * pUSARTx, uint8_t ch);
-void Usart_SendString( USART_TypeDef * pUSARTx, char *str);
-void Usart_SendHalfWord( USART_TypeDef * pUSARTx, uint16_t ch);
+void USART1_Config(void);
+void Usart1_SendByte( USART_TypeDef * pUSARTx, uint8_t ch);
+void Usart1_SendString( USART_TypeDef * pUSARTx, char *str);
+void Usart1_SendHalfWord( USART_TypeDef * pUSARTx, uint16_t ch);
 
-#endif /* __USART_H */
+#endif /* __USART1_H */
